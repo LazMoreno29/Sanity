e88b553b9442c10a61fc7bcfc0fa0935f733494f
Lazaro Moreno <laz@thecaselygroup.com>
2022-12-15 13:39:54 -0500

    created first draft for tlu webinars hosting system

---
+++ tlu-blogs/schemas/blog.ts
@@ -0,0 +1,64 @@
+export default {
+    name: 'webinar',
+    type: 'document',
+    title: 'Webinar',
+    fields: [
+        {
+            name: 'webinar_title',
+            type: 'string',
+            title: 'Webinar Title'
+        },
+        {
+            name: 'trial_professors',
+            type: 'string',
+            title: 'Trial Professors'
+
+        },
+        {
+            name: 'description',
+            title: 'Webinar Description',
+            type: 'array',
+            of:[
+                {
+                    type:'block',
+                    
+                },
+                {
+                    type:'image'
+                }
+            ]
+
+        },
+        
+        {
+            name:  'trial_professor_image_1',
+            type: 'image',
+            title: 'Trial Professor #1'
+        },
+        {
+            name:  'trial_professor_image_2',
+            type: 'image',
+            title: 'Trial Professor #2'
+        },
+        {
+            name:  'trial_professor_image_3',
+            type: 'image',
+            title: 'Trial Professor #3'
+        },
+        {
+            name:  'trial_professor_image_4',
+            type: 'image',
+            title: 'Trial Professor #4'
+        },
+        {
+            name:  'trial_professor_image_5',
+            type: 'image',
+            title: 'Trial Professor #5'
+        },
+        {
+            name: 'time',
+            type: 'datetime',
+            title: 'time'
+        }
+    ]
+}
