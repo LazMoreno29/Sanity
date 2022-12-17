0824ec201e9e7bf2a0c378f997199ae40f8e36fc
Lazaro Moreno <laz@thecaselygroup.com>
2022-12-16 19:25:03 -0500

    fixed the image component connection with sanity

--- tlu-webinar-client/pages/index.tsx
+++ tlu-webinar-client/pages/index.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
+
 import { createClient } from 'next-sanity';
+import imageUrlBuilder from '@sanity/image-url';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Home.module.css';
 
 export default function Home({ webinar }) {
-  console.log(webinar[0].description[0].children[0].text);
+  console.log(webinar);
   return (
     <>
       <div className={styles.container}>
@@ -16,7 +19,15 @@ export default function Home({ webinar }) {
             <div className='col-lg-4'>
               <h4>{webinar[0].description[0].children[0].text}</h4>
             </div>
-            <div className='col-lg-4'>column</div>
+            <div className='col-lg-4'>
+              <Image
+                loader={myLoader}
+                src={urlFor(webinar[0].trial_professor_image_1).url()}
+                alt='Edward Ciarimboli'
+                width={200}
+                height={200}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -31,9 +42,19 @@ const client = createClient({
   useCdn: false,
 });
 
+const builder = imageUrlBuilder(client);
+
+const myLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+function urlFor(source) {
+  return builder.image(source);
+}
+
 export async function getStaticProps() {
   const webinar = await client.fetch(`*[_type == "webinar"]`);
-  console.log(webinar._id);
+
   return {
     props: {
       webinar,
